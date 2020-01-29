5e08e2fc0807c4f43372005559f64fccc00ac214
Ray Zyphs <bruferviveiros@gmail.com>
2020-01-28 22:59:03 -0200

    Iniciando rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
 
-import backgroundImage from './pattern.png'
+import backgroundImage from './pattern.png';
 
 function App() {
   return (
-    <div className='App' style={{background: `url(${backgroundImage})`}}>
-      <NavBar />
-      <div className='container'>
-        <Dashboard />
+    <Router>
+      <div className='App' style={{ background: `url(${backgroundImage})` }}>
+        <NavBar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route exact path='/pokemon/:pokemonIndex' component={Pokemon} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
